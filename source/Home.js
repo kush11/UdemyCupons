9a1c8c98c3e2986175273989568ccae17e3ee8ab
kush11 <kush.codingmart.com>
2020-05-17 18:15:24 +0530

    native base added to project

--- source/Home.js
+++ source/Home.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-const HomePage = () => {
-  return (
-    <View>
-      <Text>HomePage</Text>
-    </View>
-  )
+import React, { Component } from "react";
+import { Container, Header, Content, Accordion } from "native-base";
+const dataArray = [
+  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
+export default class AccordionIconExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content padder>
+          <Accordion dataArray={dataArray} icon="add" expandedIcon="remove" />
+        </Content>
+      </Container>
+    );
+  }
 }
-
-export default HomePage
